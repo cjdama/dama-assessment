a0e4906838756b3c732c48afefa72d3389b181a5
cjdama <damacrisciait1b@gmail.com>
2021-10-13 21:04:51 +0800

    My 2nd Commit

--- python/exer3.py
+++ python/exer3.py
@@ -5,10 +5,15 @@ name2 = input('Enter your partner\'s name: ').lower().replace(" ","").replace(".
 def flames(name1, name2):
     n1 = sum(letter in name2 for letter in name1)
     n2 = sum(letter in name1 for letter in name2)
-    results = ["Friends", "Love", "Affection", "Marriage", "Enemy", "Siblings"]
-    sum_n = n1 + n2
-    print("Total: ",n1," + ",n2," = ",sum_n)
-    index = sum_n % len(results) - 1
+
+    results = ["FRIENDS", "LOVE", "ACQUAINTANCES", "MARRIAGE", "ENEMY", "SIBLINGS"]
+
+    print(name1, "---->", n1, " = " , results[n1%len(results)-1])
+    print(name2, "----> ", n2, " = " , results[n2%len(results)-1])
+    
+    counter = n1 + n2
+    print("Total: ",n1," + ",n2," = ",counter)
+    index = counter % len(results) - 1
     return results[index]
 
 print('Your Relationship is', flames(name1, name2))
